15bca8d4d7f979e685d3610f0d564655a27db1a4
Roberto Delgado <roberto.delgado@gmail.com>
2018-05-14 06:45:40 +0000

    Javascript lecture 78

---
+++ lecture78.js
@@ -0,0 +1,7 @@
+//Javascript practice
+
+var first = prompt("Enter first number");
+var second = prompt("Enter second number");
+var sum = Number(first) + Number(second);
+alert("The sum is:" + sum);
+
